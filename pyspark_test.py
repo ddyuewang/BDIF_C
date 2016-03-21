3438362caabd74401d0c81d61e87632cbc92ca32
ddyuewang <quant.yuewang@gmail.com>
2016-03-20 23:55:37 -0400

    updated version

--- pyspark_test.py
+++ pyspark_test.py
@@ -1,17 +1,25 @@
+# Load the proper libary - the main function used here is pyspark and its SQLContext
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import col
 
 sqlContext = SQLContext(sc)
 
-# given an input string file, here has to be thru hadoop put -fs first
+# given an input string file, here has to be thru hadoop put -fs first, the need to source the hadoop directory
 test_20 = sqlContext.read.json("tweets/yue_test.json")
+# test_200 = sqlContext.read.json("tweets/yue_test_200.json")
 
-test_200 = sqlContext.read.json("tweets/yue_test_200.json")
+# register the TempTable for later usage - serve like a table
+test_20.registerTempTable("test_20")
 
+### data scrubbing
 
-test_2000.registerTempTable("test_2000")
-＃filter based on language
-en_tweet = sqlContext.sql("SELECT * FROM test_2000 WHERE lang = 'en' AND test like "%apple%" ")     
+# filter based on language
+en_tweet = sqlContext.sql("SELECT * FROM test_20 WHERE lang = 'en'")
+en_tweet.registerTempTable("en_tweet")
+
+# filter based on keywords search
+apple_en_tweet = sqlCOntext.sql("SELECT * FROM test_20 WHERE text like '%apple%' or text like '%Apple%' or like 'macbook' or like ")
+
 
 ## create the corrected version
 sqlContext.sql("SELECT * FROM people WHERE lang = 'en' AND  text like '%not%'").show()
