9c1761ceb184b8ff78f4494600789686e75301ef
ddyuewang <quant.yuewang@gmail.com>
2016-03-20 21:27:44 -0400

    updated version

---
+++ pyspark_test.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SQLContext
+from pyspark.sql.functions import col
+
+sqlContext = SQLContext(sc)
+
+# given an input string file, here has to be thru hadoop put -fs first 
+test_2000 = sqlContext.read.json("tweets/yue_test_2000.json")
+
+
+test_2000.registerTempTable("test_2000")
+＃filter based on language
+en_tweet = sqlContext.sql("SELECT * FROM test_2000 WHERE lang = 'en' AND test like "%apple%" ")     
+
+## create the corrected version
+sqlContext.sql("SELECT * FROM people WHERE lang = 'en' AND  text like '%not%'").show()
+
+people.filter((people['lang'] == "en")|(people['lang'] == "fr"))
+t1 = people.where(col("lang").isin({"en", "ja"}))
+
